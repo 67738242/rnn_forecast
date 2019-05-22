759bfd927db7f556d2b96ad381b6776885bbe69a
正木　健太 <masaki@masakiktanoiMac.Z3615>
2019-05-22 17:21:29 +0900

    learning late = 0.01

--- RNN_python/rnn_attention_nocuda_git.py
+++ RNN_python/rnn_attention_nocuda_git.py
@@ -29,7 +29,7 @@ from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 #start = time.time()
 
-learning_rate = 0.001
+learning_rate = 0.01
 
 learning_data_day_len = 10
 input_digits = 24 * 5
