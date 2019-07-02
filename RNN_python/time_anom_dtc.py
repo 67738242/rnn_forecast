79563f5a2eb783bc4b82ae432030c275131fb04a
67738242 <masaki@c.info.eng.osaka-cu.ac.jp>
2019-07-02 15:41:03 +0900

    add data

--- RNN_python/time_anom_dtc.py
+++ RNN_python/time_anom_dtc.py
@@ -7,13 +7,14 @@ import os
 from statistics import mean, median,variance,stdev
 import numpy as np
 
-path_error = '/tmp/RNN_python/learning_length=10num_units50output_data_test/seq2seq_error_p_h.xlsx'
-
+# path_error = '/tmp/RNN_python/learning_length=10num_units50output_data_test/seq2seq_error_p_h.xlsx'
+path_error = '/tmp/RNN_python/output_data_sarima/s_arima_err_p_h_data.xlsx'
 
 error_p_h_data = pd.read_excel(
     path_error,
     header=0,
-    index_col=[0,1,2]
+    # index_col=[0,1,2]
+    index_col=0
 )
 
 error_p_h = error_p_h_data.values.reshape(-1)
@@ -21,9 +22,10 @@ error_p_h = error_p_h_data.values.reshape(-1)
 lkhd = []
 
 for i in range(len(error_p_h)):
-    lkhd.append(norm.pdf(error_p_h[i], loc = 0, scale = 30))
+    lkhd.append(norm.pdf(error_p_h[i], loc = 0, scale = 50))
 
 print(lkhd)
 
 lkhd_data= pd.DataFrame(lkhd)
-lkhd_data.to_csv('/tmp/RNN_python/evaluate_data1/lkhd_p_h.csv')
+# lkhd_data.to_excel('/tmp/RNN_python/evaluate_data1/lkhd_p_h_50.xlsx')
+lkhd_data.to_excel('/tmp/RNN_python/evaluate_data1/lkhd_p_h_sarima.xlsx')
